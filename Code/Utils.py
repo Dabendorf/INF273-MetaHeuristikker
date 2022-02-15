04013e6887e25f2ee022c45c36b89dc431c40ad8
Lukas Schramm <lukas@dorpapst.de>
2022-02-15 13:30:45 +0100

    Add LaTeX write line method

--- Code/Utils.py
+++ Code/Utils.py
@@ -506,8 +506,30 @@ def blind_search_latex_generator(problem: dict(), num_of_iterations: int = 10000
 			f.write(f"sol = {str(best_solution[0:150])[:-1]},\n")
 			f.write(f"      {str(best_solution[150:])[1:]}\n")
 		f.write(f"seeds = {seeds}\n")
-		f.write("\end{lstlisting}\n")
+		f.write("\end{lstlisting}%Call\_{num_calls}\_Vehicle\_{num_vehicles}\n")
 		f.write("\clearpage")
 		f.write("\n\n\n")
 
 		logging.info(f"Finished to write a LaTeX table of random solutions for file call\_{num_calls}\_vehicle\_{num_vehicles}")
+
+def latex_add_line(num_vehicles: int, num_calls: int, method:str, average_obj: float, best_obj: int, improvement: float, running_time: float):
+	""" This method adds another element into the LaTeX results table"""
+
+	logging.debug("Start to write a new line into LaTeX table")
+	path_file = "solution_table.tex"
+
+	with open(path_file, "r") as f:
+		contents = f.readlines()
+
+	try:
+		idx = contents.index(f"\end{{tabular}}%Call\_{num_calls}\_Vehicle\_{num_vehicles}\n")
+	except:
+		logging.error(f"ValueError: their is no table for the file Call_{num_calls}_Vehicle_{num_vehicles}")
+	
+
+	new_line = f"{method} & {average_obj:.2f} & {best_obj} & {improvement:.2f}\% & {running_time:.2f}s\\\\\n"
+	contents.insert(idx, new_line)
+
+	with open(path_file, "w") as f:
+		contents = "".join(contents)
+		f.write(contents)
---
+++ Code/run_localSearch.py
@@ -0,0 +1,25 @@
+from Utils import *
+
+import logging
+
+def main():
+	logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+	logger = logging.getLogger(__name__)
+	logger.disabled = False
+
+	test_files = ["../Data/Call_7_Vehicle_3.txt", "../Data/Call_18_Vehicle_5.txt", "../Data/Call_35_Vehicle_7.txt", "../Data/Call_80_Vehicle_20.txt", "../Data/Call_130_Vehicle_40.txt", "../Data/Call_300_Vehicle_90.txt"]
+	"""for test_f in test_files:
+		file = test_f
+		prob = load_problem(file)
+		logger.info(f"Problem reading finished, file {file}")
+
+		feasiblity, rand_sol, cost, counter = blind_random_search(prob)
+		logger.info(f"Generate solution with blind search")
+		logger.info(f"Solution: {rand_sol}")
+		logger.info(f"Costs: {cost}")
+		blind_search_latex_generator(prob)"""
+
+	latex_add_line(num_vehicles=3, num_calls=7, method="Local Search", average_obj=38323.2, best_obj=37001, improvement=20.5, running_time=2.5)
+
+if __name__ == "__main__":
+	main()
