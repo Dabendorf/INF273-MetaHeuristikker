bfb83fd22b69379023584f5af12f4c2bd2b4c103
Lukas Schramm <lukas@dorpapst.de>
2022-02-17 15:01:24 +0100

    Add tests for 3-exchange

--- Code/test_heuristics.py
+++ Code/test_heuristics.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from Utils import load_problem, feasibility_check, cost_function, initial_solution
-from Heuristics import alter_solution_2exchange, local_search, alter_solution_1insert
+from Heuristics import alter_solution_2exchange, local_search, alter_solution_1insert, alter_solution_3exchange
 import pytest
 import random
 	
@@ -20,13 +20,13 @@ class HeursticsValidation(TestCase):
 			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
 		assert len(init_sol) == len(alter_solution_1insert(pytest.problem_file, init_sol, 0.8))
 
-	def test_alter_solution_1insert4a(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[4])
+	def test_alter_solution_1insert3a(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[3])
 		init_sol = initial_solution(problem=pytest.problem_file)
 		assert len(init_sol) == len(alter_solution_1insert(pytest.problem_file, init_sol, 0.8))
 	
-	def test_alter_solution_1insert4b(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[4])
+	def test_alter_solution_1insert3b(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[3])
 		init_sol = initial_solution(problem=pytest.problem_file)
 		for i in range(10):
 			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
@@ -48,15 +48,15 @@ class HeursticsValidation(TestCase):
 			init_sol = alter_solution_2exchange(pytest.problem_file, init_sol)
 		assert len(init_sol) == len(alter_solution_2exchange(pytest.problem_file, init_sol))
 
-	def test_alter_solution_2exchange5a(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[5])
+	def test_alter_solution_2exchange4a(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[4])
 		init_sol = initial_solution(problem=pytest.problem_file)
 		for i in range(10):
 			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
 		assert len(init_sol) == len(alter_solution_2exchange(pytest.problem_file, init_sol))
 
-	def test_alter_solution_2exchange5b(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[5])
+	def test_alter_solution_2exchange4b(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[4])
 		init_sol = initial_solution(problem=pytest.problem_file)
 		for i in range(10):
 			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
@@ -64,23 +64,42 @@ class HeursticsValidation(TestCase):
 			init_sol = alter_solution_2exchange(pytest.problem_file, init_sol)
 		assert len(init_sol) == len(alter_solution_2exchange(pytest.problem_file, init_sol))
 
-	"""def test_costs_file0(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[0])
-		sol = [0, 2, 2, 0, 1, 5, 5, 3, 1, 3, 0, 7, 4, 6, 7, 4, 6]
-		cost = cost_function(sol, pytest.problem_file)
+	def test_alter_solution_3exchange2a(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[2])
+		init_sol = initial_solution(problem=pytest.problem_file)
+		for i in range(10):
+			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
+		for i in range(10):
+			init_sol = alter_solution_2exchange(pytest.problem_file, init_sol)
+		assert len(init_sol) == len(alter_solution_3exchange(pytest.problem_file, init_sol))
 
-		assert cost == 1871372
+	def test_alter_solution_3exchange2b(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[2])
+		init_sol = initial_solution(problem=pytest.problem_file)
+		for i in range(10):
+			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
+		for i in range(10):
+			init_sol = alter_solution_2exchange(pytest.problem_file, init_sol)
+		for i in range(10):
+			init_sol = alter_solution_3exchange(pytest.problem_file, init_sol)
+		assert len(init_sol) == len(alter_solution_3exchange(pytest.problem_file, init_sol))
 
-	def test_feasible_file0(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[0])
-		sol = [5, 5, 0, 7, 7, 0, 1, 1, 0, 4, 4, 3, 3, 6, 6, 2, 2]
-		feasiblity, _ = feasibility_check(sol, pytest.problem_file)
+	def test_alter_solution_3exchange5a(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[5])
+		init_sol = initial_solution(problem=pytest.problem_file)
+		for i in range(10):
+			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
+		for i in range(10):
+			init_sol = alter_solution_2exchange(pytest.problem_file, init_sol)
+		assert len(init_sol) == len(alter_solution_3exchange(pytest.problem_file, init_sol))
 
-		assert feasiblity == True
-
-	def test_not_feasible_file0(self):
-		pytest.problem_file = load_problem(pytest.paths_testfiles[0])
-		sol = [1, 1, 0, 7, 7, 0, 5, 5, 0, 6, 6, 2, 2, 4, 4, 3, 3]
-		feasiblity, _ = feasibility_check(sol, pytest.problem_file)
-
-		assert feasiblity == False"""
+	def test_alter_solution_3exchange5b(self):
+		pytest.problem_file = load_problem(pytest.paths_testfiles[5])
+		init_sol = initial_solution(problem=pytest.problem_file)
+		for i in range(10):
+			init_sol = alter_solution_1insert(pytest.problem_file, init_sol, 0.8)
+		for i in range(10):
+			init_sol = alter_solution_2exchange(pytest.problem_file, init_sol)
+		for i in range(10):
+			init_sol = alter_solution_3exchange(pytest.problem_file, init_sol)
+		assert len(init_sol) == len(alter_solution_3exchange(pytest.problem_file, init_sol))
